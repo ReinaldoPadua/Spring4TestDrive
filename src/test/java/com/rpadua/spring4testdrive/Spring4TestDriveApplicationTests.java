c43707a37f0064c5a1c53933793d855a793c8356
Reinaldo M PAdua <reinaldo.oak@apte.com.br>
2025-11-26 22:17:00 -0300

    feat: fix test

--- src/test/java/com/rpadua/spring4testdrive/Spring4TestDriveApplicationTests.java
+++ src/test/java/com/rpadua/spring4testdrive/Spring4TestDriveApplicationTests.java
@@ -2,8 +2,10 @@ package com.rpadua.spring4testdrive;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Spring4TestDriveApplicationTests {
 
     @Test
